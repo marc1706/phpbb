70280c631bdb20d4fa1cb44997c45ef60a7655c0
Marc Alexander <admin@m-a-styles.de>
2025-12-24 10:36:08 +0100

    [feature/tailwind] Extend base css and tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,107 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-	content: [ './phpBB/styles/chameleon/**/*.{html,js}' ],
+import type { Config } from "tailwindcss";
+
+export default {
+	darkMode: ['class'],
+	content: ['./phpBB/styles/chameleon/**/*.{html,js}'],
+	prefix: "",
 	theme: {
+		container: {
+			center: true,
+			padding: "2rem",
+			screens: {
+				"2xl": "1400px",
+			},
+		},
 		extend: {
 			colors: {
-				white: '#ffffff',
-				cosmic: {
-					DEFAULT: '#009BDF',
-					50: '#98DFFF',
-					100: '#83D9FF',
-					200: '#5ACDFF',
-					300: '#32C0FF',
-					400: '#09B4FF',
-					500: '#009BDF',
-					600: '#0074A7',
-					700: '#004D6F',
-					800: '#002637',
-					900: '#000000',
-					950: '#000000'
+				border: "hsl(var(--border))",
+				input: "hsl(var(--input))",
+				ring: "hsl(var(--ring))",
+				background: "hsl(var(--background))",
+				foreground: "hsl(var(--foreground))",
+				primary: {
+					DEFAULT: "hsl(var(--primary))",
+					foreground: "hsl(var(--primary-foreground))",
 				},
-				odyssey: '#505f79',
-				cosmos: '#29303d',
+				secondary: {
+					DEFAULT: "hsl(var(--secondary))",
+					foreground: "hsl(var(--secondary-foreground))",
+				},
+				destructive: {
+					DEFAULT: "hsl(var(--destructive))",
+					foreground: "hsl(var(--destructive-foreground))",
+				},
+				muted: {
+					DEFAULT: "hsl(var(--muted))",
+					foreground: "hsl(var(--muted-foreground))",
+				},
+				accent: {
+					DEFAULT: "hsl(var(--accent))",
+					foreground: "hsl(var(--accent-foreground))",
+				},
+				popover: {
+					DEFAULT: "hsl(var(--popover))",
+					foreground: "hsl(var(--popover-foreground))",
+				},
+				card: {
+					DEFAULT: "hsl(var(--card))",
+					foreground: "hsl(var(--card-foreground))",
+				},
+				sidebar: {
+					DEFAULT: "hsl(var(--sidebar-background))",
+					foreground: "hsl(var(--sidebar-foreground))",
+					primary: "hsl(var(--sidebar-primary))",
+					"primary-foreground": "hsl(var(--sidebar-primary-foreground))",
+					accent: "hsl(var(--sidebar-accent))",
+					"accent-foreground": "hsl(var(--sidebar-accent-foreground))",
+					border: "hsl(var(--sidebar-border))",
+					ring: "hsl(var(--sidebar-ring))",
+				},
+				cosmic: {
+					DEFAULT: 'hsl(198, 100%, 44%)',
+					50: 'hsl(199, 100%, 80%)',
+					100: 'hsl(198, 100%, 76%)',
+					200: 'hsl(198, 100%, 68%)',
+					300: 'hsl(198, 100%, 60%)',
+					400: 'hsl(198, 100%, 52%)',
+					500: 'hsl(198, 100%, 44%)',
+					600: 'hsl(198, 100%, 33%)',
+					700: 'hsl(198, 100%, 22%)',
+					800: 'hsl(199, 100%, 11%)',
+					900: 'hsl(0, 0%, 0%)',
+					950: 'hsl(0, 0%, 0%)'
+				},
+				odyssey: 'hsl(218, 20%, 39%)',
+				cosmos: 'hsl(219, 20%, 20%)',
+			},
+			borderRadius: {
+				lg: "var(--radius)",
+				md: "calc(var(--radius) - 2px)",
+				sm: "calc(var(--radius) - 4px)",
+			},
+			keyframes: {
+				"accordion-down": {
+					from: {
+						height: "0",
+					},
+					to: {
+						height: "var(--radix-accordion-content-height)",
+					},
+				},
+				"accordion-up": {
+					from: {
+						height: "var(--radix-accordion-content-height)",
+					},
+					to: {
+						height: "0",
+					},
+				},
+			},
+			animation: {
+				"accordion-down": "accordion-down 0.2s ease-out",
+				"accordion-up": "accordion-up 0.2s ease-out",
 			},
 		},
 	},
-	plugins: [],
-};
+	plugins: [require("tailwindcss-animate")],
+} satisfies Config;

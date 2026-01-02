efd63f567ba48bcb66fd49fd76b44e0fe5c79f95
Marc Alexander <admin@m-a-styles.de>
2026-01-02 13:19:05 +0100

    [feature/tailwind] Remove postcss-sorting and add autoprefixer and cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,7 @@
-const sorting = require('postcss-sorting');
-const sortOrder = require('./.postcss-sorting.json');
-
 module.exports = {
 	plugins: [
 		require('tailwindcss')('./tailwind.config.js'),
 		require('autoprefixer')(),
-		sorting(sortOrder),
 		process.env.MINIFY === '1' ? require('cssnano')() : false,
 	].filter(Boolean),
 };

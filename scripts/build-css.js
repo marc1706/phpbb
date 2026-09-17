87037846b3b447fa006bc02f6d1278c848d46c5e
Marc Alexander <admin@m-a-styles.de>
2026-09-17 15:58:05 +0100

    [feature/tailwind] Adapt scripts for neptune

--- scripts/build-css.js
+++ scripts/build-css.js
@@ -3,8 +3,8 @@
 const { spawnSync } = require('child_process');
 
 // Build CSS: phpBB/styles/chameleon/theme/main.css -> stylesheet.css
-const input = 'phpBB/styles/chameleon/theme/main.css';
-const output = 'phpBB/styles/chameleon/theme/stylesheet.css';
+const input = 'phpBB/styles/neptune/theme/main.css';
+const output = 'phpBB/styles/neptune/theme/stylesheet.css';
 
 const res = spawnSync('postcss', [ input, '-o', output, '--map' ], { stdio: 'inherit', shell: true, env: process.env });
 process.exit(res.status === null ? 1 : res.status);

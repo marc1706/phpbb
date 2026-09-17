7223630d36ae746b76df03fc419f667e58ef0750
Marc Alexander <admin@m-a-styles.de>
2026-09-17 15:58:05 +0100

    [feature/tailwind] Adapt scripts for neptune

--- scripts/watch-css.js
+++ scripts/watch-css.js
@@ -3,8 +3,8 @@
 const { spawn } = require('child_process');
 
 // Watch CSS: watch source main.css and output stylesheet.css
-const input = 'phpBB/styles/chameleon/theme/main.css';
-const output = 'phpBB/styles/chameleon/theme/stylesheet.css';
+const input = 'phpBB/styles/neptune/theme/main.css';
+const output = 'phpBB/styles/neptune/theme/stylesheet.css';
 
 const ps = spawn('postcss', [ input, '-o', output, '--watch', '--map', '--verbose' ], { stdio: 'inherit', shell: true, env: process.env });
 
